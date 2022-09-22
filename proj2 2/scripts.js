86df8c29531d0283a2cb7dbc9917c58f81f1e676
Vijay Rawat <30477642+vijay-rawat@users.noreply.github.com>
2022-09-22 19:42:11 +0530

    Add files via upload

---
+++ proj2 2/scripts.js
@@ -0,0 +1,28 @@
+const red = document.querySelector(".red");
+const cyan = document.querySelector(".cyan");
+const violet = document.querySelector(".violet");
+const orange = document.querySelector(".orange");
+const pink = document.querySelector(".pink");
+
+const center = document.querySelector(".center");
+
+//console.log(window.getComputedStyle(red).backgroundColor);
+
+const getBGColor = (selectedElement) => {
+  return window.getComputedStyle(selectedElement).backgroundColor;
+};
+var color = getBGColor(pink);
+pink.addEventListener("mouseenter", () => {
+  center.style.background = color;
+});
+
+const magicColorChanger = (element, color) => {
+  return element.addEventListener("mouseenter", () => {
+    center.style.background = color;
+  });
+};
+magicColorChanger(red, getBGColor(red));
+magicColorChanger(cyan, getBGColor(cyan));
+magicColorChanger(orange, getBGColor(orange));
+magicColorChanger(violet, getBGColor(violet));
+magicColorChanger(pink, getBGColor(pink));
